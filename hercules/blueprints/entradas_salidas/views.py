5521eaf706420acae7af4036d1a9f8d443e590ac
Guillermo Valdes <guillermo@movimientolibre.com>
2024-04-28 17:57:59 -0600

    Filtros en bitacoras y entradas_salidas

--- hercules/blueprints/entradas_salidas/views.py
+++ hercules/blueprints/entradas_salidas/views.py
@@ -5,10 +5,12 @@ Entradas-Salidas
 from flask import Blueprint, render_template, request, url_for
 from flask_login import login_required
 
+from lib.safe_string import safe_email
 from lib.datatables import get_datatable_parameters, output_datatable_json
 from hercules.blueprints.entradas_salidas.models import EntradaSalida
 from hercules.blueprints.permisos.models import Permiso
 from hercules.blueprints.usuarios.decorators import permission_required
+from hercules.blueprints.usuarios.models import Usuario
 
 MODULO = "ENTRADAS SALIDAS"
 
@@ -29,10 +31,22 @@ def datatable_json():
     draw, start, rows_per_page = get_datatable_parameters()
     # Consultar
     consulta = EntradaSalida.query
+    # Primero filtrar por columnas propias
     if "estatus" in request.form:
         consulta = consulta.filter_by(estatus=request.form["estatus"])
     else:
         consulta = consulta.filter_by(estatus="A")
+    if "usuario_id" in request.form:
+        consulta = consulta.filter_by(usuario_id=request.form["usuario_id"])
+    # Luego filtrar por columnas de otras tablas
+    if "usuario_email" in request.form:
+        try:
+            usuario_email = safe_email(request.form["usuario_email"], search_fragment=True)
+            if usuario_email != "":
+                consulta = consulta.join(Usuario).filter(Usuario.email.contains(usuario_email))
+        except ValueError:
+            pass
+    # Ordenar y paginar
     registros = consulta.order_by(EntradaSalida.id.desc()).offset(start).limit(rows_per_page).all()
     total = consulta.count()
     # Elaborar datos para DataTable
