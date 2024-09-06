9d7750870cc0a423a28721dfe62e9fa010226999
Lucia Aranda <lucyarandal@gmail.com>
2024-09-06 15:16:54 -0600

    Entrega de faltante sentencias

---
+++ hercules/blueprints/sentencias/forms.py
@@ -0,0 +1,51 @@
+"""
+Sentencias, formularios
+"""
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileRequired
+from wtforms import BooleanField, DateField, FileField, IntegerField, SelectField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional, Regexp
+
+from lib.safe_string import EXPEDIENTE_REGEXP, SENTENCIA_REGEXP
+
+
+class SentenciaNewForm(FlaskForm):
+    """Formulario SentenciaNew"""
+
+    distrito = StringField("Distrito")  # Read only
+    autoridad = StringField("Autoridad")  # Read only
+    sentencia = StringField("Sentencia", validators=[DataRequired(), Length(max=16), Regexp(SENTENCIA_REGEXP)])
+    sentencia_fecha = DateField("Fecha de la sentencia", validators=[DataRequired()])
+    expediente = StringField("Expediente", validators=[DataRequired(), Length(max=16), Regexp(EXPEDIENTE_REGEXP)])
+    fecha = DateField("Fecha de publicación", validators=[DataRequired()])
+    materia = SelectField("Materia", choices=None, validate_choice=False)  # Las opciones se agregan con JS
+    materia_tipo_juicio = SelectField("Tipo de Juicio", choices=None, validate_choice=False)  # Las opciones se agregan con JS
+    descripcion = TextAreaField("Descripción", validators=[Optional(), Length(max=1024)])
+    es_perspectiva_genero = BooleanField("Es Perspectiva de Género", validators=[Optional()])
+    archivo = FileField("Archivo PDF", validators=[FileRequired()])
+    guardar = SubmitField("Guardar")
+
+
+class SentenciaEditForm(FlaskForm):
+    """Formulario para editar Sentencia"""
+
+    sentencia = StringField("Sentencia", validators=[DataRequired(), Length(max=16), Regexp(SENTENCIA_REGEXP)])
+    sentencia_fecha = DateField("Fecha de la sentencia", validators=[Optional()])
+    expediente = StringField("Expediente", validators=[DataRequired(), Length(max=16), Regexp(EXPEDIENTE_REGEXP)])
+    fecha = DateField("Fecha de publicación", validators=[DataRequired()])
+    materia = SelectField("Materia", choices=None, validate_choice=False)  # Las opciones se agregan con JS
+    materia_tipo_juicio = SelectField("Tipo de Juicio", choices=None, validate_choice=False)  # Las opciones se agregan con JS
+    descripcion = TextAreaField("Descripción", validators=[Optional(), Length(max=1024)])
+    es_perspectiva_genero = BooleanField("Es Perspectiva de Género", validators=[Optional()])
+    guardar = SubmitField("Guardar")
+
+
+class SentenciaReportForm(FlaskForm):
+    """Formulario para elaborar reporte de listas de acuerdos"""
+
+    autoridad_id = IntegerField("Autoridad ID", validators=[DataRequired()])
+    fecha_desde = DateField("Desde", validators=[DataRequired()])
+    fecha_hasta = DateField("Hasta", validators=[DataRequired()])
+    por_tipos_de_juicios = BooleanField("Por tipos de juicios", validators=[Optional()])
+    elaborar = SubmitField("Elaborar")
---
+++ hercules/blueprints/sentencias/task.py
@@ -0,0 +1,158 @@
+"""
+Sentencias, tareas para ejecutar en el fondo
+"""
+
+import base64
+import csv
+import locale
+import logging
+import os
+import random
+from datetime import date, datetime
+from pathlib import Path
+
+import sendgrid
+from dotenv import load_dotenv
+from sendgrid.helpers.mail import (  # type: ignore
+    Attachment,
+    Content,
+    ContentId,
+    Disposition,
+    Email,
+    FileContent,
+    FileName,
+    FileType,
+    Mail,
+    To,
+)
+
+from hercules.app import create_app
+from hercules.blueprints.sentencias.models import Sentencia
+from lib.tasks import set_task_error, set_task_progress
+
+load_dotenv()  # Take environment variables from .env
+
+bitacora = logging.getLogger(__name__)
+bitacora.setLevel(logging.INFO)
+formato = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
+empunadura = logging.FileHandler("logs/sentencias.log")
+empunadura.setFormatter(formato)
+bitacora.addHandler(empunadura)
+
+app = create_app()
+app.app_context().push()
+
+locale.setlocale(locale.LC_TIME, "es_MX.utf8")
+
+
+def enviar_reporte():
+    """Enviar via correo electronico el reporte de sentencias"""
+
+    # Fecha
+    fecha = date.today()
+
+    # Iniciar tarea
+    mensaje_inicial = f"Inicia enviar reporte de sentencias del {fecha}"
+    set_task_progress(0, mensaje_inicial)
+    bitacora.info(mensaje_inicial)
+
+    # Momento en que se elabora este reporte
+    momento = datetime.now()
+    momento_str = momento.strftime("%d/%B/%Y %I:%M%p")
+    subject = f"Sentencias del {fecha.strftime('%d/%B/%Y')}"
+
+    # Definir la ruta al archivo temporal CSV
+    random_hex = "%030x" % random.randrange(16**30)
+    reporte_ruta = Path("/tmp/reporte-sentencias-" + random_hex + ".csv")
+    reporte_archivo = f"reporte-sentencias-{fecha.strftime('%Y-%m-%d')}.csv"
+
+    # Cabecera
+    bitacora.info("Inicia enviar reporte de %s", momento_str)
+    contenidos = [
+        f"<h1>PJECZ Plataforma Web</h1><h2>{subject}</h2><p>Fecha de elaboración: {momento_str}.<br>ESTE MENSAJE ES ELABORADO POR UN PROGRAMA. FAVOR DE NO RESPONDER.</p>"
+    ]
+
+    # Contenido
+    sentencias = Sentencia.query.order_by(Sentencia.id.desc()).limit(50).all()
+    if len(sentencias) == 0:
+        bitacora.warning("No hay sentencias")
+    else:
+        contador = 0
+        with open(reporte_ruta, "w", encoding="utf8") as puntero:
+            reporte = csv.writer(puntero)
+            reporte.writerow(
+                [
+                    "ID",
+                    "Autoridades",
+                    "Fechas",
+                    "Sentencias",
+                    "Expedientes",
+                    "Materias",
+                    "Tipos de Juicios",
+                    "URLs",
+                ]
+            )
+            for sentencia in sentencias:
+                reporte.writerow(
+                    [
+                        sentencia.id,
+                        sentencia.autoridad.clave,
+                        sentencia.fecha,
+                        sentencia.sentencia,
+                        sentencia.expediente,
+                        sentencia.materia_tipo_juicio.materia.nombre,
+                        sentencia.materia_tipo_juicio.descripcion,
+                        sentencia.url,
+                    ]
+                )
+                contador += 1
+        bitacora.info("Se han escrito %d sentencias en el archivo CSV.", contador)
+
+    # Si no hay contenido, no enviar nada
+    if contador == 0:
+        mensaje = "No se va enviar nada porque no hay sentencias."
+        bitacora.info(mensaje)
+    else:
+        # Preparar el mensaje para manadr con SendGrid
+        api_key = os.getenv("SENDGRID_API_KEY", "")
+        if api_key == "":
+            mensaje = "No se pudo enviar el reporte de listas de acuerdos porque no esta definida la variable de entorno SENDGRID_API_KEY"
+            bitacora.warning(mensaje)
+            return set_task_error(mensaje)
+        sendgrid_client = sendgrid.SendGridAPIClient(api_key=api_key)
+        from_str = os.getenv("SENDGRID_FROM_EMAIL", "")
+        if from_str == "":
+            mensaje = "No se pudo enviar el reporte de listas de acuerdos porque no esta definida la variable de entorno SENDGRID_FROM_EMAIL"
+            bitacora.warning(mensaje)
+            return set_task_error(mensaje)
+        from_email = Email(from_str)
+        to_str = os.getenv("SENDGRID_TO_EMAIL_REPORTES", "")
+        if to_str == "":
+            mensaje = "No se pudo enviar el reporte de listas de acuerdos porque no esta definida la variable de entorno SENDGRID_TO_EMAIL_REPORTES"
+            bitacora.warning(mensaje)
+            return set_task_error(mensaje)
+        to_email = To(to_str)
+        content = Content("text/html", "<br>".join(contenidos))
+        mail = Mail(from_email, to_email, subject, content)
+        # Adjuntar el archivo CSV
+        with open(reporte_ruta, "r", encoding="utf8") as puntero:
+            datos = puntero.read()
+            puntero.close()
+        encoded = base64.b64encode(bytes(datos, "utf8")).decode("utf8")
+        attachment = Attachment()
+        attachment.file_content = FileContent(encoded)
+        attachment.file_type = FileType("text/csv")
+        attachment.file_name = FileName(reporte_archivo)
+        attachment.disposition = Disposition("attachment")
+        attachment.content_id = ContentId("ArchivoCSV")
+        mail.attachment = attachment
+        # Enviar el mensaje
+        sendgrid_client.client.mail.send.post(request_body=mail.get())
+        # Eliminar el archivo CSV
+        reporte_ruta.unlink(missing_ok=True)
+
+    # Terminar tarea
+    mensaje_final = f"Termina enviar reporte de sentencias del {fecha}"
+    set_task_progress(100, mensaje_final)
+    bitacora.info(mensaje_final)
+    return mensaje_final
