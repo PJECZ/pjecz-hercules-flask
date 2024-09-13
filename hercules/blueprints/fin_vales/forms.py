804c3ad33821f59565a5679aed015ca27f6bcaa4
Guillermo Valdes <guillermo@movimientolibre.com>
2024-09-13 10:22:10 -0600

    Avance en Vales de Gasolina. Falta tareas en el fondo.

---
+++ hercules/blueprints/fin_vales/forms.py
@@ -0,0 +1,94 @@
+"""
+Financieros Vales, formularios
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, PasswordField, SelectField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional
+
+from hercules.blueprints.fin_vales.models import FinVale
+
+MONTOS = [
+    ("100.0", "$100.00"),
+    ("200.0", "$200.00"),
+]
+
+
+class FinValeEditForm(FlaskForm):
+    """Formulario para editar un vale"""
+
+    usuario_email = SelectField(
+        label="Cambiar al usuario con su e-mail", coerce=str, validators=[Optional()], validate_choice=False
+    )
+    solicito_email = SelectField(
+        label="Cambiar al solicitante con su e-mail", coerce=str, validators=[Optional()], validate_choice=False
+    )
+    autorizo_email = SelectField(
+        label="Cambiar al autorizante con su e-mail", coerce=str, validators=[Optional()], validate_choice=False
+    )
+    justificacion = TextAreaField("Justificación", validators=[DataRequired(), Length(max=1024)], render_kw={"rows": 4})
+    monto = SelectField("Monto", choices=MONTOS, validators=[DataRequired()])
+    actualizar = SubmitField("Actualizar")
+
+
+class FinValeStep1CreateForm(FlaskForm):
+    """Formulario paso 1 crear FinVale"""
+
+    justificacion = TextAreaField("Justificación", validators=[DataRequired(), Length(max=1024)], render_kw={"rows": 4})
+    monto = SelectField("Monto", choices=MONTOS, validators=[DataRequired()])
+    guardar = SubmitField("Guardar")
+
+
+class FinValeStep2RequestForm(FlaskForm):
+    """Formulario paso 2 solicitar FinVale"""
+
+    contrasena = PasswordField("Contraseña de su firma electrónica", validators=[DataRequired(), Length(6, 64)])
+    solicitar = SubmitField("Solicitar")
+
+
+class FinValeCancel2RequestForm(FlaskForm):
+    """Formulario cancelar solicitado FinVale"""
+
+    motivo = StringField("Motivo", validators=[DataRequired(), Length(max=256)])
+    contrasena = PasswordField("Contraseña de su firma electrónica", validators=[DataRequired(), Length(6, 64)])
+    cancelar = SubmitField("Cancelar")
+
+
+class FinValeStep3AuthorizeForm(FlaskForm):
+    """Formulario paso 3 autorizar FinVale"""
+
+    contrasena = PasswordField("Contraseña de su firma electrónica", validators=[DataRequired(), Length(6, 64)])
+    autorizar = SubmitField("Autorizar")
+
+
+class FinValeCancel3AuthorizeForm(FlaskForm):
+    """Formulario cancelar autorizado FinVale"""
+
+    motivo = StringField("Motivo", validators=[DataRequired(), Length(max=256)])
+    contrasena = PasswordField("Contraseña de su firma electrónica", validators=[DataRequired(), Length(6, 64)])
+    cancelar = SubmitField("Cancelar")
+
+
+class FinValeStep4DeliverForm(FlaskForm):
+    """Formulario paso 4 entregar FinVale"""
+
+    folio = IntegerField("Folio", validators=[DataRequired()])
+    entregar = SubmitField("Entregar")
+
+
+class FinValeStep5AttachmentsForm(FlaskForm):
+    """Formulario paso 5 adjuntar FinVale"""
+
+    vehiculo_descripcion = StringField("Descripción del vehículo", validators=[DataRequired(), Length(max=256)])
+    tanque_inicial = StringField("Tanque inicial", validators=[Optional(), Length(max=48)])
+    tanque_final = StringField("Tanque final", validators=[Optional(), Length(max=48)])
+    kilometraje_inicial = IntegerField("Kilometraje inicial", validators=[Optional()])
+    kilometraje_final = IntegerField("Kilometraje final", validators=[Optional()])
+    concluir = SubmitField("Concluir entrega de adjuntos")
+
+
+class FinValeStep6ArchiveForm(FlaskForm):
+    """Formulario paso 6 archivar FinVale"""
+
+    notas = TextAreaField("Notas", validators=[DataRequired(), Length(max=1020)])
+    archivar = SubmitField("Archivar")
--- hercules/blueprints/fin_vales/models.py
+++ hercules/blueprints/fin_vales/models.py
@@ -17,13 +17,13 @@ class FinVale(database.Model, UniversalMixin):
 
     ESTADOS = {
         "CREADO": "Creado",  # PASO 1 Un usuario lo ha creado
-        "SOLICITADO": "Solicitado",  # PASO 2 El superior lo autorizo con su firma
-        "CANCELADO POR SOLICITANTE": "Cancelado por solicitante",  # El superior ha cancelado la firma
-        "AUTORIZADO": "Autorizado",  # PASO 3 Finanzas lo autorizo
-        "CANCELADO POR AUTORIZANTE": "Cancelado por autorizante",  # Finanzas ha cancelado la firma
-        "ENTREGADO": "Entregado",  # PASO 4 El usuario lo recogió
-        "POR REVISAR": "Por revisar",  # PASO 5 El usuario a subido los archivos adjuntos
-        "ARCHIVADO": "Archivado",  # PASO 6 Finanzas lo archiva si cumple con la evidencia
+        "SOLICITADO": "Solicitado",  # PASO 2 El solicitante lo ha firmado
+        "CANCELADO POR SOLICITANTE": "Cancelado por solicitante",  # El solicitante lo ha cancelado
+        "AUTORIZADO": "Autorizado",  # PASO 3 El autorizante lo ha firmado
+        "CANCELADO POR AUTORIZANTE": "Cancelado por autorizante",  # El autorizante lo ha cancelado
+        "ENTREGADO": "Entregado",  # PASO 4 El vale ha sido entregado
+        "POR REVISAR": "Por revisar",  # PASO 5 Se han subido los archivos como evidencia
+        "ARCHIVADO": "Archivado",  # PASO 6 Se ha archivado
     }
 
     TIPOS = {
@@ -41,13 +41,13 @@ class FinVale(database.Model, UniversalMixin):
     usuario_id: Mapped[int] = mapped_column(ForeignKey("usuarios.id"))
     usuario: Mapped["Usuario"] = relationship(back_populates="fin_vales")
 
-    # Columnas (step 1 create) estado CREADO
+    # Columnas CREADO: PASO 1 Un usuario lo ha creado
     estado: Mapped[str] = mapped_column(Enum(*ESTADOS, name="fin_vales_estados", native_enum=False), index=True)
     tipo: Mapped[str] = mapped_column(Enum(*TIPOS, name="fin_vales_tipos", native_enum=False), index=True)
     justificacion: Mapped[str] = mapped_column(Text())
     monto: Mapped[float]
 
-    # Columnas (step 2 request) estado SOLICITADO
+    # Columnas SOLICITADO: PASO 2 El solicitante lo ha firmado
     solicito_nombre: Mapped[Optional[str]] = mapped_column(String(256))
     solicito_puesto: Mapped[Optional[str]] = mapped_column(String(256))
     solicito_email: Mapped[Optional[str]] = mapped_column(String(256))
@@ -59,12 +59,12 @@ class FinVale(database.Model, UniversalMixin):
     solicito_efirma_mensaje: Mapped[Optional[str]] = mapped_column(String(512))
     solicito_efirma_error: Mapped[Optional[str]] = mapped_column(String(512))
 
-    # Columnas (cancel 2 request) estado CANCELADO POR SOLICITANTE
+    # Columnas CANCELADO POR SOLICITANTE: El solicitante lo ha cancelado
     solicito_cancelo_tiempo: Mapped[Optional[datetime]]
     solicito_cancelo_motivo: Mapped[Optional[str]] = mapped_column(String(256))
     solicito_cancelo_error: Mapped[Optional[str]] = mapped_column(String(512))
 
-    # Columnas (step 3 authorize) estado AUTORIZADO
+    # Columnas AUTORIZADO: PASO 3 El autorizante lo ha firmado
     autorizo_nombre: Mapped[Optional[str]] = mapped_column(String(256))
     autorizo_puesto: Mapped[Optional[str]] = mapped_column(String(256))
     autorizo_email: Mapped[Optional[str]] = mapped_column(String(256))
@@ -76,22 +76,22 @@ class FinVale(database.Model, UniversalMixin):
     autorizo_efirma_mensaje: Mapped[Optional[str]] = mapped_column(String(512))
     autorizo_efirma_error: Mapped[Optional[str]] = mapped_column(String(512))
 
-    # Columnas (cancel 3 authorize) estado CANCELADO POR AUTORIZANTE
+    # Columnas CANCELADO POR AUTORIZANTE: El autorizante lo ha cancelado
     autorizo_cancelo_tiempo: Mapped[Optional[datetime]]
     autorizo_cancelo_motivo: Mapped[Optional[str]] = mapped_column(String(256))
     autorizo_cancelo_error: Mapped[Optional[str]] = mapped_column(String(512))
 
-    # Columnas (step 4 deliver) estado ENTREGADO
+    # Columnas ENTREGADO: PASO 4 El vale ha sido entregado
     folio: Mapped[Optional[int]]
 
-    # Columnas (step 5 attachments) estado POR REVISAR
+    # Columnas POR REVISAR: PASO 5 Se han subido los archivos como evidencia
     vehiculo_descripcion: Mapped[Optional[str]] = mapped_column(String(256))
     tanque_inicial: Mapped[Optional[str]] = mapped_column(String(48))
     tanque_final: Mapped[Optional[str]] = mapped_column(String(48))
     kilometraje_inicial: Mapped[Optional[int]]
     kilometraje_final: Mapped[Optional[int]]
 
-    # Columnas (step 6 archive) estado ARCHIVADO
+    # Columnas ARCHIVADO: PASO 6 Se ha archivado
     notas: Mapped[Optional[str]] = mapped_column(String(1024))
 
     def __repr__(self):
