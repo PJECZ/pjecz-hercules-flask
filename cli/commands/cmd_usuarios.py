574b3edf9f75f7e7cc8da7303260ecf4f47d20dc
ricval <rcvaldes@gmail.com>
2025-01-10 15:31:36 -0600

    Integración del reporte de usuarios de PW a Hercules

--- cli/commands/cmd_usuarios.py
+++ cli/commands/cmd_usuarios.py
@@ -4,10 +4,14 @@ CLI Usuarios
 - mostrar_api_key: Mostrar la API Key de un usuario
 - nueva_api_key: Nueva API Key
 - nueva_contrasena: Nueva contraseña
+- reporte: Reporte de usuarios modificados a un archivo CSV
 """
 
 import sys
+import re
+import csv
 from datetime import datetime, timedelta
+from pathlib import Path
 
 import click
 
@@ -77,6 +81,79 @@ def nueva_contrasena(email):
     click.echo(f"Se ha cambiado la contraseña de {email} en usuarios")
 
 
+@click.command()
+@click.argument("desde", type=str)
+@click.argument("hasta", type=str)
+@click.option("--output", default="reporte_usuarios.csv", type=str, help="Archivo CSV a escribir")
+def reporte(desde, hasta, output):
+    """Reporte de usuarios modificados a un archivo CSV"""
+    # Validar el archivo CSV a escribir, que no exista
+    ruta = Path(output)
+    if ruta.exists():
+        click.echo(f"AVISO: {output} existe, no voy a sobreescribirlo.")
+        return
+    # Validar que el parametro desde sea YYYY-MM-DD
+    if not re.match(r"^\d{4}-\d{2}-\d{2}$", desde):
+        click.echo(f"ERROR: {desde} no es una fecha valida (YYYY-MM-DD)")
+        return
+    # Validar que el parametro hasta sea YYYY-MM-DD
+    if not re.match(r"^\d{4}-\d{2}-\d{2}$", hasta):
+        click.echo(f"ERROR: {hasta} no es una fecha valida (YYYY-MM-DD)")
+        return
+    # Validar que la fecha desde sea menor que la fecha hasta
+    if desde > hasta:
+        click.echo(f"ERROR: {desde} es mayor que {hasta}")
+        return
+    # Consultar usuarios
+    click.echo("Elaborando reporte de usuarios...")
+    contador = 0
+    usuarios = Usuario.query
+    if desde:
+        usuarios = usuarios.filter(Usuario.modificado >= f"{desde} 00:00:00")
+    if hasta:
+        usuarios = usuarios.filter(Usuario.modificado <= f"{hasta} 23:59:59")
+    usuarios = usuarios.order_by(Usuario.modificado).all()
+    with open(ruta, "w", encoding="utf8") as puntero:
+        respaldo = csv.writer(puntero)
+        respaldo.writerow(
+            [
+                "Fecha",
+                "Nombre Completo",
+                "CURP",
+                "Correo Electrónico",
+                "Autoridad",
+                "Oficina",
+                "Roles",
+                "Operacion",
+            ]
+        )
+        for usuario in usuarios:
+            # Definir operacion
+            operacion = "ALTA"  # Por defecto, es ALTA
+            if usuario.estatus == "B":
+                operacion = "BAJA"  # Si estatus es B, es BAJA
+            elif usuario.modificado - usuario.creado > timedelta(hours=24):
+                operacion = "CAMBIO"  # Si modificado tiene mas de 24 horas respecto a creado, es CAMBIO
+            # Escribir la linea
+            respaldo.writerow(
+                [
+                    usuario.modificado.strftime("%Y-%m-%d %H:%M:%S"),
+                    usuario.nombre,
+                    usuario.curp,
+                    usuario.email,
+                    usuario.autoridad.clave,
+                    usuario.oficina.clave,
+                    ", ".join(usuario.get_roles()),
+                    operacion,
+                ]
+            )
+            contador += 1
+            if contador % 100 == 0:
+                click.echo(f"  Van {contador}...")
+    click.echo(f"  {contador} en {ruta.name}")
+
+
 cli.add_command(mostrar_api_key)
 cli.add_command(nueva_api_key)
 cli.add_command(nueva_contrasena)
+cli.add_command(reporte)
