573a4bf70f597aff6836a088db805a4f74963856
Guillermo Valdes <guillermo@movimientolibre.com>
2025-02-26 08:32:38 -0600

    Enviar actualizacion ya corregido por id

--- hercules/blueprints/exh_exhortos_actualizaciones/tasks.py
+++ hercules/blueprints/exh_exhortos_actualizaciones/tasks.py
@@ -13,14 +13,14 @@ app.app_context().push()
 database.app = app
 
 
-def task_enviar_actualizacion(exhorto_origen_id: str) -> str:
+def task_enviar_actualizacion(exh_exhorto_actualizacion_id: int) -> str:
     """Lanzar tarea en el fondo para enviar una actualización"""
     # Iniciar la tarea en el fondo
     set_task_progress(0, "Se ha lanzado la tarea en el fondo para enviar una actualización")
 
     # Ejecutar
     try:
-        mensaje_termino, nombre_archivo, url_publica = enviar_actualizacion(exhorto_origen_id)
+        mensaje_termino, nombre_archivo, url_publica = enviar_actualizacion(exh_exhorto_actualizacion_id)
     except MyAnyError as error:
         mensaje_error = str(error)
         set_task_error(mensaje_error)
