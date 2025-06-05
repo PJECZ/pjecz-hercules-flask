be0fa1021bbde1058f2aaf4f3144dccc09605a02
ricval <rcvaldes@gmail.com>
2025-06-05 15:02:24 -0600

    FIX: Relaciones de modelos

--- hercules/blueprints/ofi_documentos_destinatarios/models.py
+++ hercules/blueprints/ofi_documentos_destinatarios/models.py
@@ -10,17 +10,17 @@ from hercules.extensions import database
 
 
 class OfiDocumentoDestinatario(database.Model, UniversalMixin):
-    """ OfiDocumentoDestinatario """
+    """OfiDocumentoDestinatario"""
 
     # Nombre de la tabla
-    __tablename__ = 'ofi_documentos_destinatarios'
+    __tablename__ = "ofi_documentos_destinatarios"
 
     # Clave primaria
     id: Mapped[int] = mapped_column(primary_key=True)
 
     # Clave foránea
     ofi_documento_id: Mapped[int] = mapped_column(ForeignKey("ofi_documentos.id"))
-    ofi_documento: Mapped["OfiDocumento"] = relationship(back_populates="ofi_documentos_adjuntos")
+    ofi_documento: Mapped["OfiDocumento"] = relationship(back_populates="ofi_documentos_destinatarios")
     usuario_id: Mapped[int] = mapped_column(ForeignKey("usuarios.id"))
     usuario: Mapped["Usuario"] = relationship(back_populates="ofi_documentos_destinatarios")
 
@@ -28,5 +28,5 @@ class OfiDocumentoDestinatario(database.Model, UniversalMixin):
     fue_leido: Mapped[bool] = mapped_column(default=False)
 
     def __repr__(self):
-        """ Representación """
-        return f'<OfiDocumentoDestinatario {self.id}>'
+        """Representación"""
+        return f"<OfiDocumentoDestinatario {self.id}>"
