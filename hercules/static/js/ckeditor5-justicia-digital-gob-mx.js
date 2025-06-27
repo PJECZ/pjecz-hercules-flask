8034f18e8a6d31f177f91d6723318a3f07a196d0
Guillermo Valdes <guillermo@movimientolibre.com>
2025-06-27 11:16:03 -0600

    Se usa la licencia GPL en CKEditor5 y se suben a GC Storage sus archivos

---
+++ hercules/static/js/ckeditor5-justicia-digital-gob-mx.js
@@ -0,0 +1,187 @@
+/**
+ * This configuration was generated using the CKEditor 5 Builder
+ */
+
+const {
+  ClassicEditor,
+  Alignment,
+  AutoLink,
+  Autosave,
+  BlockQuote,
+  Bold,
+  Bookmark,
+  Code,
+  CodeBlock,
+  Essentials,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  Paragraph,
+  PlainTableOutput,
+  RemoveFormat,
+  Strikethrough,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableLayout,
+  TableProperties,
+  TableToolbar,
+  Underline,
+} = window.CKEDITOR;
+
+/**
+ * Using the GPL key
+ * Use the 'GPL' value in the licenseKey field in your configuration.
+ */
+const LICENSE_KEY = "GPL";
+
+const editorConfig = {
+  toolbar: {
+    items: [
+      "undo",
+      "redo",
+      "|",
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "underline",
+      "strikethrough",
+      "code",
+      "removeFormat",
+      "|",
+      "horizontalLine",
+      "link",
+      "bookmark",
+      "insertTable",
+      "insertTableLayout",
+      "highlight",
+      "blockQuote",
+      "codeBlock",
+      "|",
+      "alignment",
+      "|",
+      "outdent",
+      "indent",
+    ],
+    shouldNotGroupWhenFull: false,
+  },
+  plugins: [
+    Alignment,
+    AutoLink,
+    Autosave,
+    BlockQuote,
+    Bold,
+    Bookmark,
+    Code,
+    CodeBlock,
+    Essentials,
+    GeneralHtmlSupport,
+    Heading,
+    Highlight,
+    HorizontalLine,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    Paragraph,
+    PlainTableOutput,
+    RemoveFormat,
+    Strikethrough,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableLayout,
+    TableProperties,
+    TableToolbar,
+    Underline,
+  ],
+  heading: {
+    options: [
+      {
+        model: "paragraph",
+        title: "Paragraph",
+        class: "ck-heading_paragraph",
+      },
+      {
+        model: "heading1",
+        view: "h1",
+        title: "Heading 1",
+        class: "ck-heading_heading1",
+      },
+      {
+        model: "heading2",
+        view: "h2",
+        title: "Heading 2",
+        class: "ck-heading_heading2",
+      },
+      {
+        model: "heading3",
+        view: "h3",
+        title: "Heading 3",
+        class: "ck-heading_heading3",
+      },
+      {
+        model: "heading4",
+        view: "h4",
+        title: "Heading 4",
+        class: "ck-heading_heading4",
+      },
+      {
+        model: "heading5",
+        view: "h5",
+        title: "Heading 5",
+        class: "ck-heading_heading5",
+      },
+      {
+        model: "heading6",
+        view: "h6",
+        title: "Heading 6",
+        class: "ck-heading_heading6",
+      },
+    ],
+  },
+  htmlSupport: {
+    allow: [
+      {
+        name: /^.*$/,
+        styles: true,
+        attributes: true,
+        classes: true,
+      },
+    ],
+  },
+  initialData: "",
+  language: "es",
+  licenseKey: LICENSE_KEY,
+  link: {
+    addTargetToExternalLinks: true,
+    defaultProtocol: "https://",
+    decorators: {
+      toggleDownloadable: {
+        mode: "manual",
+        label: "Downloadable",
+        attributes: {
+          download: "file",
+        },
+      },
+    },
+  },
+  placeholder: "Escribe o copia y pega tu texto aquí...",
+  table: {
+    contentToolbar: [
+      "tableColumn",
+      "tableRow",
+      "mergeTableCells",
+      "tableProperties",
+      "tableCellProperties",
+    ],
+  },
+};
