a7bc8c5635d682c7dac96f4f99305cc766f72246
Guillermo Valdes <guillermo@movimientolibre.com>
2025-06-30 05:06:49 -0600

    Oficios Documentos lista la estructura para tareas en el fondo. Ya se mandan emails a los destinatarios.

--- hercules/blueprints/ofi_documentos/communications/send_to_sendgrid.py
+++ hercules/blueprints/ofi_documentos/communications/send_to_sendgrid.py
@@ -15,13 +15,14 @@ from hercules.blueprints.ofi_documentos.communications import bitacora
 from hercules.blueprints.ofi_documentos.models import OfiDocumento
 from hercules.blueprints.ofi_documentos_destinatarios.models import OfiDocumentoDestinatario
 from hercules.extensions import database
-from lib.exceptions import MyNotExistsError, MyNotValidParamError
+from lib.exceptions import MyIsDeletedError, MyMissingConfigurationError, MyNotExistsError, MyNotValidParamError
+from lib.safe_string import safe_uuid
 
 # Cargar variables de entorno
 load_dotenv()
-HOST = os.getenv("HOST", "https://plataforma-web.justiciadigital.gob.mx")
+HOST = os.getenv("HOST", "http://localhost:5000")
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", "")
-SENDGRID_FROM_EMAIL = os.getenv("SENDGRID_FROM_EMAIL", "plataforma.web@pjecz.gob.mx")
+SENDGRID_FROM_EMAIL = os.getenv("SENDGRID_FROM_EMAIL", "")
 TZ = os.getenv("TZ", "America/Mexico_City")
 
 # Cargar la aplicación para tener acceso a la base de datos
@@ -35,22 +36,29 @@ TIMEOUT = 60  # segundos
 def enviar_a_sendgrid(ofi_documento_id: int) -> tuple[str, str, str]:
     """Enviar un mensaje por Sendgrid"""
     mensajes = []
-    mensaje_info = "Inicia enviar un mensaje por Sendgrid."
+    mensaje_info = f"Inicia enviar un mensaje por Sendgrid el oficio {ofi_documento_id}"
     mensajes.append(mensaje_info)
     bitacora.info(mensaje_info)
 
     # TODO: Validar que esté definida la variable de entorno SENDGRID_API_KEY
+    if not SENDGRID_API_KEY:
+        raise MyMissingConfigurationError("La variable de entorno SENDGRID_API_KEY no está definida")
 
-    # Consultar y validar el oficio
-    ofi_documento = OfiDocumento.query.filter(OfiDocumento.id == ofi_documento_id).first()
+    # TODO: Validar que esté definida la variable de entorno SENDGRID_FROM_EMAIL
+    if not SENDGRID_FROM_EMAIL:
+        raise MyMissingConfigurationError("La variable de entorno SENDGRID_FROM_EMAIL no está definida")
+
+    # Consultar el oficio
+    ofi_documento_id = safe_uuid(ofi_documento_id)
+    if not ofi_documento_id:
+        raise MyNotValidParamError("ID de oficio inválido")
+    ofi_documento = OfiDocumento.query.get(ofi_documento_id)
     if not ofi_documento:
-        mensaje = f"El Oficio {ofi_documento_id} no existe"
-        bitacora.error(mensaje)
-        raise MyNotExistsError(mensaje)
+        raise MyNotExistsError("El oficio no existe")
+
+    # Validar el estatus, que no esté eliminado
     if ofi_documento.estatus != "A":
-        mensaje = f"El Oficio {ofi_documento_id} no está activo"
-        bitacora.error(mensaje)
-        raise MyNotValidParamError(mensaje)
+        raise MyIsDeletedError("El oficio está eliminado")
 
     # Consultar los destinatarios
     ofi_destinatarios = (
@@ -69,7 +77,7 @@ def enviar_a_sendgrid(ofi_documento_id: int) -> tuple[str, str, str]:
     contenidos.append(f"<h2>{asunto_str}</h2>")
     contenidos.append(f"<p>Enviado el {fecha_envio}</p>")
     contenidos.append("<ul>")
-    contenidos.append(f"<li>Remitente: <strong>{ofi_documento.usuario.nombre}</strong></li>")
+    contenidos.append(f"<li>Remitente: <strong>{ofi_documento.usuario.nombre}</strong>, {ofi_documento.usuario.puesto}</li>")
     contenidos.append(f"<li>Folio: <strong>{ofi_documento.folio}</strong></li>")
     contenidos.append(f"<li>Descripción: <strong>{ofi_documento.descripcion}</strong></li>")
     contenidos.append("</ul>")
@@ -77,8 +85,8 @@ def enviar_a_sendgrid(ofi_documento_id: int) -> tuple[str, str, str]:
     contenidos.append("<ul>")
     contenidos.append(f"<li><a href='{HOST}/ofi_documentos/{ofi_documento.id}'>Oficio {ofi_documento.id}</a></li>")
     contenidos.append("</ul>")
-    contenidos.append("<p>Este mensaje fue elaborado por un programa. <strong>NO RESPONDA ESTE MENSAJE.</strong></p>")
-    contenidos.append("<p>Para lograr el CERO PAPEL por favor <strong>NO IMPRIMA ESTE MENSAJE ni el Oficio.</strong></p>")
+    contenidos.append("<p>Para lograr la meta <em>CERO PAPEL</em> por favor <em>NO IMPRIMA ESTE MENSAJE ni el Oficio.</em></p>")
+    contenidos.append("<p>Este mensaje fue enviado por un programa. <em>NO RESPONDA ESTE MENSAJE.</em></p>")
     contenido_html = "\n".join(contenidos)
 
     # Enviar el e-mail
@@ -91,12 +99,12 @@ def enviar_a_sendgrid(ofi_documento_id: int) -> tuple[str, str, str]:
         html_content=contenido,
     )
     for destinatario in ofi_destinatarios:
-        if destinatario.con_copia == True:
-            mail.add_cc(destinatario.usuario.email)
+        if destinatario.con_copia is False:
+            mail.add_to(destinatario.usuario.email)
             mensajes.append(f"- Destinatario: {destinatario.usuario.email}")
         else:
-            mail.add_to(destinatario.usuario.email)
-            mensajes.append(f"- Destinatario con copia: {destinatario.usuario.email}")
+            mail.add_cc(destinatario.usuario.email)
+            mensajes.append(f"- Con copia: {destinatario.usuario.email}")
 
     # Enviar mensaje de correo electrónico
     send_grid.client.mail.send.post(request_body=mail.get())
--- hercules/blueprints/ofi_documentos/communications/send_to_whatsapp.py
+++ hercules/blueprints/ofi_documentos/communications/send_to_whatsapp.py
@@ -6,8 +6,10 @@ import requests
 
 from hercules.app import create_app
 from hercules.blueprints.ofi_documentos.communications import bitacora
+from hercules.blueprints.ofi_documentos.models import OfiDocumento
 from hercules.extensions import database
-from lib.exceptions import MyAnyError, MyConnectionError, MyEmptyError, MyNotExistsError, MyNotValidAnswerError
+from lib.exceptions import MyConnectionError, MyNotExistsError, MyIsDeletedError, MyNotValidParamError
+from lib.safe_string import safe_uuid
 
 app = create_app()
 app.app_context().push()
@@ -19,9 +21,21 @@ TIMEOUT = 60  # segundos
 def enviar_a_whatsapp(ofi_documento_id: int) -> tuple[str, str, str]:
     """Enviar un mensaje por WhatsApp"""
     mensajes = []
-    mensaje_info = "Inicia enviar un mensaje por WhatsApp."
+    mensaje_info = f"Inicia enviar un mensaje por WhatsApp el oficio {ofi_documento_id}"
     mensajes.append(mensaje_info)
     bitacora.info(mensaje_info)
+
+    # Consultar el oficio
+    ofi_documento_id = safe_uuid(ofi_documento_id)
+    if not ofi_documento_id:
+        raise MyNotValidParamError("ID de oficio inválido")
+    ofi_documento = OfiDocumento.query.get(ofi_documento_id)
+    if not ofi_documento:
+        raise MyNotExistsError("El oficio no existe")
+
+    # Validar el estatus, que no esté eliminado
+    if ofi_documento.estatus != "A":
+        raise MyIsDeletedError("El oficio está eliminado")
 
     # Elaborar mensaje_termino
     mensaje_termino = f"Termina enviar un mensaje por WhatsApp."
