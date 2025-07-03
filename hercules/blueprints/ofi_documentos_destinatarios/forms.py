5696f173599b389c528df3175efc79a5e647058b
ricval <rcvaldes@gmail.com>
2025-07-03 14:47:56 -0600

    Vista de Agregar Destinatarios

--- hercules/blueprints/ofi_documentos_destinatarios/forms.py
+++ hercules/blueprints/ofi_documentos_destinatarios/forms.py
@@ -11,6 +11,8 @@ class OfiDocumentoDestinatarioForm(FlaskForm):
     """Formulario OfiDocumentoDestinatario"""
 
     ofi_documento = StringField("Oficio")  # Read-Only
-    usuario = SelectField("Usuario", coerce=int, validate_choice=False, validators=[DataRequired()])
+    usuario = SelectField("Usuario", coerce=int, validate_choice=False, validators=[Optional()])
+    autoridad = SelectField("Autoridad", coerce=int, validate_choice=False, validators=[Optional()])
     con_copia = BooleanField("Con Copia", validators=[Optional()])
+    con_copia_autoridad = BooleanField("Con Copia", validators=[Optional()])
     guardar = SubmitField("Guardar")
