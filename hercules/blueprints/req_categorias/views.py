2efe3b1163a8a9e70808dbace1f7555665f31c18
ricval <rcvaldes@gmail.com>
2025-07-10 11:16:08 -0600

    Req Categorías

--- hercules/blueprints/req_categorias/views.py
+++ hercules/blueprints/req_categorias/views.py
@@ -1,5 +1,5 @@
 """
-Requisiciones Categorías, vistas
+Req Categorías, vistas
 """
 
 import json
@@ -7,13 +7,15 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from lib.datatables import get_datatable_parameters, output_datatable_json
-from lib.safe_string import safe_string, safe_message
+from lib.safe_string import safe_string, safe_message, safe_clave
 
 from hercules.blueprints.bitacoras.models import Bitacora
 from hercules.blueprints.modulos.models import Modulo
 from hercules.blueprints.permisos.models import Permiso
 from hercules.blueprints.usuarios.decorators import permission_required
 from hercules.blueprints.req_categorias.models import ReqCategoria
+from hercules.blueprints.req_categorias.forms import ReqCategoriaForm
+
 
 MODULO = "REQ CATEGORIAS"
 
@@ -25,3 +27,173 @@ req_categorias = Blueprint("req_categorias", __name__, template_folder="template
 @permission_required(MODULO, Permiso.VER)
 def before_request():
     """Permiso por defecto"""
+
+
+@req_categorias.route("/req_categorias/datatable_json", methods=["GET", "POST"])
+def datatable_json():
+    """DataTable JSON para listado de Req Categorías"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = ReqCategoria.query
+    # Primero filtrar por columnas propias
+    if "estatus" in request.form:
+        consulta = consulta.filter_by(estatus=request.form["estatus"])
+    else:
+        consulta = consulta.filter_by(estatus="A")
+    if "clave" in request.form:
+        clave = safe_clave(request.form["clave"])
+        if clave:
+            consulta = consulta.filter(ReqCategoria.clave.contains(clave))
+    if "descripcion" in request.form:
+        descripcion = safe_string(request.form["descripcion"])
+        if descripcion:
+            consulta = consulta.filter(ReqCategoria.descripcion.contains(descripcion))
+    # Ordenar y paginar
+    registros = consulta.order_by(ReqCategoria.id).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for resultado in registros:
+        data.append(
+            {
+                "detalle": {
+                    "clave": resultado.clave,
+                    "url": url_for("req_categorias.detail", req_categoria_id=resultado.id),
+                },
+                "descripcion": resultado.descripcion,
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
+
+
+@req_categorias.route("/req_categorias")
+def list_active():
+    """Listado de Req Categorías activos"""
+    return render_template(
+        "req_categorias/list.jinja2",
+        filtros=json.dumps({"estatus": "A"}),
+        titulo="Requisiciones Categorías",
+        estatus="A",
+    )
+
+
+@req_categorias.route("/req_categorias/inactivos")
+@permission_required(MODULO, Permiso.ADMINISTRAR)
+def list_inactive():
+    """Listado de Req Categorías inactivos"""
+    return render_template(
+        "req_categorias/list.jinja2",
+        filtros=json.dumps({"estatus": "B"}),
+        titulo="Requisiciones Categorías inactivas",
+        estatus="B",
+    )
+
+
+@req_categorias.route("/req_categorias/<int:req_categoria_id>")
+def detail(req_categoria_id):
+    """Detalle de un Req Categoría"""
+    req_categoria = ReqCategoria.query.get_or_404(req_categoria_id)
+    return render_template("req_categorias/detail.jinja2", req_categoria=req_categoria)
+
+
+@req_categorias.route("/req_categorias/nuevo", methods=["GET", "POST"])
+@permission_required(MODULO, Permiso.CREAR)
+def new():
+    """Nuevo Req Categoría"""
+    form = ReqCategoriaForm()
+    if form.validate_on_submit():
+        es_valido = True
+        # Validar que la clave no exista
+        clave = safe_clave(form.clave.data)
+        req_categoria_repetida = ReqCategoria.query.filter_by(clave=clave).first()
+        if req_categoria_repetida:
+            flash(f"La clave '{clave}' ya existe", "warning")
+            es_valido = False
+        if es_valido:
+            req_categoria = ReqCategoria(
+                clave=clave,
+                descripcion=safe_string(form.descripcion.data),
+            )
+            req_categoria.save()
+            bitacora = Bitacora(
+                modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+                usuario=current_user,
+                descripcion=safe_message(f"Nueva Req Categoría {req_categoria.clave}"),
+                url=url_for("req_categorias.detail", req_categoria_id=req_categoria.id),
+            )
+            bitacora.save()
+            flash(bitacora.descripcion, "success")
+            return redirect(bitacora.url)
+    return render_template("req_categorias/new.jinja2", form=form)
+
+
+@req_categorias.route("/req_categorias/edicion/<int:req_categoria_id>", methods=["GET", "POST"])
+@permission_required(MODULO, Permiso.MODIFICAR)
+def edit(req_categoria_id):
+    """Editar Req Categoría"""
+    req_categoria = ReqCategoria.query.get_or_404(req_categoria_id)
+    form = ReqCategoriaForm()
+    if form.validate_on_submit():
+        es_valido = True
+        # Validar que la clave no esté repetida
+        clave = safe_clave(form.clave.data)
+        clave_repetida = ReqCategoria.query.filter_by(clave=clave).filter(ReqCategoria.id != req_categoria.id).first()
+        if clave_repetida:
+            flash(f"La clave '{clave}' ya está en uso.", "warning")
+            es_valido = False
+        if es_valido:
+            req_categoria.clave = clave
+            req_categoria.descripcion = safe_string(form.descripcion.data)
+            req_categoria.save()
+            bitacora = Bitacora(
+                modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+                usuario=current_user,
+                descripcion=safe_message(f"Editado Req Categoría {req_categoria.clave}"),
+                url=url_for("req_categorias.detail", req_categoria_id=req_categoria.id),
+            )
+            bitacora.save()
+            flash(bitacora.descripcion, "success")
+            return redirect(bitacora.url)
+    # Cargar datos
+    form.clave.data = req_categoria.clave
+    form.descripcion.data = req_categoria.descripcion
+    # Entregar
+    return render_template("req_categorias/edit.jinja2", form=form, req_categoria=req_categoria)
+
+
+@req_categorias.route("/req_categorias/eliminar/<int:req_categoria_id>")
+@permission_required(MODULO, Permiso.ADMINISTRAR)
+def delete(req_categoria_id):
+    """Eliminar Req Categoría"""
+    req_categoria = ReqCategoria.query.get_or_404(req_categoria_id)
+    if req_categoria.estatus == "A":
+        req_categoria.delete()
+        bitacora = Bitacora(
+            modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+            usuario=current_user,
+            descripcion=safe_message(f"Eliminado Req Categoría {req_categoria.clave}"),
+            url=url_for("req_categorias.detail", req_categoria_id=req_categoria.id),
+        )
+        bitacora.save()
+        flash(bitacora.descripcion, "success")
+    return redirect(url_for("req_categorias.detail", req_categoria_id=req_categoria.id))
+
+
+@req_categorias.route("/req_categorias/recuperar/<int:req_categoria_id>")
+@permission_required(MODULO, Permiso.ADMINISTRAR)
+def recover(req_categoria_id):
+    """Recuperar Req Categoría"""
+    req_categoria = ReqCategoria.query.get_or_404(req_categoria_id)
+    if req_categoria.estatus == "B":
+        req_categoria.recover()
+        bitacora = Bitacora(
+            modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+            usuario=current_user,
+            descripcion=safe_message(f"Recuperado Req Categoría {req_categoria.clave}"),
+            url=url_for("req_categorias.detail", req_categoria_id=req_categoria.id),
+        )
+        bitacora.save()
+        flash(bitacora.descripcion, "success")
+    return redirect(url_for("req_categorias.detail", req_categoria_id=req_categoria.id))
--- hercules/blueprints/req_requisiciones/views.py
+++ hercules/blueprints/req_requisiciones/views.py
@@ -25,3 +25,69 @@ req_requisiciones = Blueprint("req_requisiciones", __name__, template_folder="te
 @permission_required(MODULO, Permiso.VER)
 def before_request():
     """Permiso por defecto"""
+
+
+@req_requisiciones.route("/req_requisiciones/datatable_json", methods=["GET", "POST"])
+def datatable_json():
+    """DataTable JSON para listado de Req Requisiciones"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = ReqRequisicion.query
+    # Primero filtrar por columnas propias
+    if "estatus" in request.form:
+        consulta = consulta.filter_by(estatus=request.form["estatus"])
+    else:
+        consulta = consulta.filter_by(estatus="A")
+    # if "persona_id" in request.form:
+    #     consulta = consulta.filter_by(persona_id=request.form["persona_id"])
+    # Luego filtrar por columnas de otras tablas
+    # if "persona_rfc" in request.form:
+    #     consulta = consulta.join(Persona)
+    #     consulta = consulta.filter(Persona.rfc.contains(safe_rfc(request.form["persona_rfc"], search_fragment=True)))
+    # Ordenar y paginar
+    registros = consulta.order_by(ReqRequisicion.id).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for resultado in registros:
+        data.append(
+            {
+                "detalle": {
+                    "nombre": resultado.nombre,
+                    "url": url_for("req_requisiciones.detail", req_requisicion_id=resultado.id),
+                },
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
+
+
+@req_requisiciones.route("/req_requisiciones")
+def list_active():
+    """Listado de Req Requisiciones activos"""
+    return render_template(
+        "req_requisiciones/list.jinja2",
+        filtros=json.dumps({"estatus": "A"}),
+        titulo="Requisiciones",
+        estatus="A",
+    )
+
+
+@req_requisiciones.route("/req_requisiciones/inactivos")
+@permission_required(MODULO, Permiso.ADMINISTRAR)
+def list_inactive():
+    """Listado de Req Requisiciones inactivos"""
+    return render_template(
+        "req_requisiciones/list.jinja2",
+        filtros=json.dumps({"estatus": "B"}),
+        titulo="Requisiciones inactivas",
+        estatus="B",
+    )
+
+
+@req_requisiciones.route("/req_requisiciones/<int:req_requisiciones_id>")
+def detail(req_requisiciones_id):
+    """Detalle de un Req Requisición"""
+    req_requisicion = ReqRequisicion.query.get_or_404(req_requisiciones_id)
+    return render_template("req_requisiciones/detail.jinja2", req_requisicion=req_requisicion)
