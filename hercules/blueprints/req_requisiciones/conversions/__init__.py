8f26af88b9334b4178fa5c555ad4f493cd801c13
ricval <rcvaldes@gmail.com>
2025-09-01 13:39:04 -0600

    cli bitacora

---
+++ hercules/blueprints/req_requisiciones/conversions/__init__.py
@@ -0,0 +1,12 @@
+"""
+Conversions init
+"""
+
+import logging
+
+bitacora = logging.getLogger(__name__)
+bitacora.setLevel(logging.INFO)
+formato = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
+empunadura = logging.FileHandler("logs/req_requisiciones_conversions.log")
+empunadura.setFormatter(formato)
+bitacora.addHandler(empunadura)
